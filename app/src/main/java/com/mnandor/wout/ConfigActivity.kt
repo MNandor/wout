3ec0837517a00c74827f6158bd87e06bfeb899d0
MNandor <mnandor@users.noreply.github.com>
2022-08-08 13:38:26 +0300

    Add RecyclerView to config screen

--- app/src/main/java/com/mnandor/wout/ConfigActivity.kt
+++ app/src/main/java/com/mnandor/wout/ConfigActivity.kt
@@ -2,10 +2,17 @@ package com.mnandor.wout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ConfigActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.exerciseTemplatesRecycle)
+        val adapter = ExerciseTemplatesAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/mnandor/wout/ExerciseTemplatesAdapter.kt
@@ -0,0 +1,42 @@
+package com.mnandor.wout
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ExerciseTemplatesAdapter : RecyclerView.Adapter<ExerciseTemplatesAdapter.ExerciseTemplateViewHolder>() {
+
+    private var items:List<ExerciseTemplate> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseTemplateViewHolder {
+        return ExerciseTemplateViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ExerciseTemplateViewHolder, position: Int) {
+        val current = items[position]
+        holder.bind(current.name)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ExerciseTemplateViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val wordItemView: TextView = itemView.findViewById(R.id.exerciseTemplateNameTV)
+
+        fun bind(text: String?) {
+            wordItemView.text = text
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): ExerciseTemplateViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_exercise_template, parent, false)
+                return ExerciseTemplateViewHolder(view)
+            }
+        }
+    }
+
+}
