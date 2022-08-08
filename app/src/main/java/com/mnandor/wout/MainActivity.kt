0edc2dcdf79a3b2b362df3eba46eea83589df6d4
MNandor <mnandor@users.noreply.github.com>
2022-08-08 11:26:22 +0300

    Add view binding

--- app/src/main/java/com/mnandor/wout/MainActivity.kt
+++ app/src/main/java/com/mnandor/wout/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.mnandor.wout
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mnandor.wout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.addButton.visibility = View.GONE
     }
 }
