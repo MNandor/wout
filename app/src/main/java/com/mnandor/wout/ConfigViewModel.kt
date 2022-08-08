bc9f876ce2efa184f580f553f170a44bcf23d332
MNandor <mnandor@users.noreply.github.com>
2022-08-08 13:20:59 +0300

    Add Config ViewModel

---
+++ app/src/main/java/com/mnandor/wout/ConfigViewModel.kt
@@ -0,0 +1,23 @@
+import androidx.lifecycle.*
+import com.mnandor.wout.ExerciseDatabase
+import com.mnandor.wout.ExerciseTemplate
+import kotlinx.coroutines.launch
+
+class ConfigViewModel(private val database: ExerciseDatabase) : ViewModel() {
+
+    val allTemplates: LiveData<List<ExerciseTemplate>> = database.dao().getTemplates().asLiveData()
+
+    fun insert(template: ExerciseTemplate) = viewModelScope.launch {
+        database.dao().addExerciseTemplate(template)
+    }
+}
+
+class ConfigViewModelFactory(private val database: ExerciseDatabase) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ConfigViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ConfigViewModel(database) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
