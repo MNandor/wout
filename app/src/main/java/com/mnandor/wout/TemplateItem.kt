105dbc60811c117cc2795fd6b0b9e608950a202e
BinaKaiser <binakaiser@users.noreply.github.com>
2022-12-13 16:24:50 +0200

    add unique constraint to exercise templates

--- app/src/main/java/com/mnandor/wout/TemplateItem.kt
+++ app/src/main/java/com/mnandor/wout/TemplateItem.kt
@@ -2,13 +2,16 @@ package com.mnandor.wout
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "day_template")
+@Entity(tableName = "day_template", indices = [Index(value = ["exercise", "template"], unique = true)])
 data class TemplateItem(
     @PrimaryKey(autoGenerate = true)
     var itemID: Int = 0,
+    @ColumnInfo(name="exercise")
     val exercise: String,
+    @ColumnInfo(name="template")
     val template: String,
 
 )
