7c2b5102b8455a22f508fe94b0c506eef0e3e0ea
MNandor <mnandor@users.noreply.github.com>
2023-05-12 00:25:58 +0300

    schedule, define adapter

--- app/src/main/java/com/mnandor/wout/presentation/ScheduleActivity.kt
+++ app/src/main/java/com/mnandor/wout/presentation/ScheduleActivity.kt
@@ -1,7 +1,9 @@
 package com.mnandor.wout.presentation
 
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +11,8 @@ import com.mnandor.wout.WoutApplication
 import com.mnandor.wout.data.entities.Exercise
 import com.mnandor.wout.databinding.ActivityConfigBinding
 import com.mnandor.wout.databinding.ActivityScheduleBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ScheduleActivity : AppCompatActivity() {
     private lateinit var binding: ActivityScheduleBinding
@@ -23,6 +27,31 @@ class ScheduleActivity : AppCompatActivity() {
         binding = ActivityScheduleBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
+        val recyclerView = binding.scheduleRecycler
+        val adapter = ScheduleAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        updateViewModel(7, 3)
+    }
+
+    private fun updateViewModel(totalDays: Int, todayIs: Int) {
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        calendar.add(Calendar.DATE, -todayIs)
+
+        for (i in 1..totalDays){
+            calendar.add(Calendar.DATE, 1)
+            val date = dateFormat.format(calendar.time)
+
+            Log.d("in-dev", date)
+
+            if (i == todayIs)
+                Log.d("in-dev", date)
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/mnandor/wout/presentation/ScheduleAdapter.kt
@@ -0,0 +1,53 @@
+package com.mnandor.wout.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mnandor.wout.R
+import com.mnandor.wout.data.entities.Exercise
+
+class ScheduleAdapter : RecyclerView.Adapter<ScheduleAdapter.ExerciseTemplateViewHolder>() {
+
+    private var items:List<Exercise> = listOf()
+
+    fun setItems(newItems:List<Exercise>){
+        items = newItems
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseTemplateViewHolder {
+        return ExerciseTemplateViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ExerciseTemplateViewHolder, position: Int) {
+        val current = items[position]
+        holder.bind(current)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ExerciseTemplateViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val wordItemView: TextView = itemView.findViewById(R.id.exerciseTemplateNameTV)
+
+        fun bind(item: Exercise) {
+            wordItemView.text = item.name
+            if (item.isDisabled){
+                wordItemView.alpha = 0.5F
+            } else {
+                wordItemView.alpha = 1F
+            }
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): ExerciseTemplateViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_exercise_template, parent, false)
+                return ExerciseTemplateViewHolder(view)
+            }
+        }
+    }
+
+}
