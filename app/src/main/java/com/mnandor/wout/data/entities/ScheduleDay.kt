fb6b61658653031fce53fda7ff711382fd13ce1a
MNandor <mnandor@users.noreply.github.com>
2023-05-15 14:44:50 +0300

    Add scheduleday to db

---
+++ app/src/main/java/com/mnandor/wout/data/entities/ScheduleDay.kt
@@ -0,0 +1,16 @@
+package com.mnandor.wout.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "scheduleday")
+data class ScheduleDay(
+    @PrimaryKey @ColumnInfo(name = "day") val day: Int,
+
+    @ColumnInfo(name="location")
+    val location: Int,
+
+    @ColumnInfo(name="exercises")
+    val exercises: String,
+)
